4f5db901a14b810e1e0163f94e58d479ed858000
Hyun jin <qnrgoqlt@naver.com>
2024-07-10 16:11:11 +0900

    스택 네비게이터

--- App.js
+++ App.js
@@ -1,124 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react';
+import HomeScreen from './screens/stack/HomeScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import DetailScreen from './screens/stack/DetailScreen';
+import TodoScreen from './screens/stack/TodoScreen';
+import {Text, TouchableOpacity, View} from 'react-native';
+import HeaderlessScreen from './screens/stack/HeaderlessScreen';
 
-import React, {useEffect, useState} from 'react';
-import {
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import DateHead from './components/DateHead';
-import AddTodo from './components/AddTodo';
-import Empty from './components/Empty';
-import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
-import TodoList from './components/TodoList';
-import todosStorage from './sotrages/todosStorage';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const today = new Date();
-
-  const [todos, setTodos] = useState([
-    {id: 1, text: '작업환경 설정', done: true},
-    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
-    {id: 3, text: '투두리스트 만들어보기', done: false},
-  ]);
-
-  // 불러오기
-  useEffect(() => {
-    todosStorage.get().then(setTodos).catch(console.error);
-  }, []);
-
-  useEffect(() => {
-    todosStorage.set(todos).catch(console.error);
-  }, [todos]);
-
-  const onInsert = text => {
-    // 새로 등록할 항목의 id를 구합니다.
-    // 등록된 항목 중에서 가장 큰 id를 구하고, 그 값에 1을 더합니다.
-    // 만약 리스트가 비어있다면 1을 id로 사용합니다.
-    const nextId =
-      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
-    const todo = {
-      id: nextId,
-      text,
-      done: false,
-    };
-
-    setTodos(todos.concat(todo));
-  };
-
-  const onRemove = id => {
-    const nextTodos = todos.filter(todo => todo.id !== id);
-    setTodos(nextTodos);
-  };
-
-  const onToggle = id => {
-    const nextTodos = todos.map(todo =>
-      todo.id === id ? {...todo, done: !todo.done} : todo,
-    );
-    setTodos(nextTodos);
-  };
-
   return (
-    <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']} style={{flex: 1}}>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-          style={{flex: 1}}>
-          <DateHead date={today} />
-          {todos.length === 0 ? (
-            <Empty />
-          ) : (
-            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
-          )}
-          <AddTodo onInsert={onInsert} />
-        </KeyboardAvoidingView>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: '홈',
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{
+            headerBackVisible: false,
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity onPress={onPress}>
+                <Text>Left</Text>
+              </TouchableOpacity>
+            ),
+            headerTitle: ({children}) => (
+              <View>
+                <Text>{children}</Text>
+              </View>
+            ),
+            headerRight: () => (
+              <View>
+                <Text>Right</Text>
+              </View>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="Headerless"
+          component={HeaderlessScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="TodoList" component={TodoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ screens/stack/HomeScreen.js
@@ -0,0 +1,28 @@
+import {View, Text, Button} from 'react-native';
+import React, {useEffect} from 'react';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Button
+        title="Detail 열기"
+        onPress={() => navigation.navigate('Detail', {id: 1})}
+        // onPress={() => navigation.push('Detail')}
+      />
+      <Button
+        title="Detail 열기"
+        onPress={() => navigation.navigate('Detail', {id: 2})}
+      />
+      <Button
+        title="Detail 열기"
+        onPress={() => navigation.navigate('Detail', {id: 3})}
+      />
+      <Button
+        title="HeaderLess페이지 열기"
+        onPress={() => navigation.navigate('Headerless')}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
